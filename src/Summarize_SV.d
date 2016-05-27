e28c97df861c12de70798f783a4a53057046e67a
fritzsedlazeck <fritz.sedalzeck@gmail.com>
2016-05-27 13:59:02 -0400

    Short update

--- src/Summarize_SV.d
+++
@@ -1,8 +0,0 @@
-src/Summarize_SV.d: ../src/Summarize_SV.cpp ../src/Summarize_SV.h \
-  ../src/vcfs/Merge_VCF.h ../src/vcfs/../structs.h
-
-../src/Summarize_SV.h:
-
-../src/vcfs/Merge_VCF.h:
-
-../src/vcfs/../structs.h:
--- src/simulator/SV_Simulator.d
+++
@@ -1,4 +0,0 @@
-src/simulator/SV_Simulator.d: ../src/simulator/SV_Simulator.cpp \
-  ../src/simulator/SV_Simulator.h
-
-../src/simulator/SV_Simulator.h:
