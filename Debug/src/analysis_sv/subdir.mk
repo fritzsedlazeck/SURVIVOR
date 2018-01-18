c90a5661813aa4d78c894319663b7a649bf7ac97
fritz.sedlazeck <fritz.sedlazeck@gmail.com>
2018-01-18 12:07:21 -0600

    Updated make file

--- Debug/src/analysis_sv/subdir.mk
+++ Debug/src/analysis_sv/subdir.mk
@@ -6,16 +6,22 @@
 CPP_SRCS += \
 ../src/analysis_sv/GIAB_summary.cpp \
 ../src/analysis_sv/MT_identifier.cpp \
+../src/analysis_sv/MUMmer_overlap.cpp \
+../src/analysis_sv/Simplify_SVs.cpp \
 ../src/analysis_sv/Summ_mat.cpp 
 
 OBJS += \
 ./src/analysis_sv/GIAB_summary.o \
 ./src/analysis_sv/MT_identifier.o \
+./src/analysis_sv/MUMmer_overlap.o \
+./src/analysis_sv/Simplify_SVs.o \
 ./src/analysis_sv/Summ_mat.o 
 
 CPP_DEPS += \
 ./src/analysis_sv/GIAB_summary.d \
 ./src/analysis_sv/MT_identifier.d \
+./src/analysis_sv/MUMmer_overlap.d \
+./src/analysis_sv/Simplify_SVs.d \
 ./src/analysis_sv/Summ_mat.d 
 
 
