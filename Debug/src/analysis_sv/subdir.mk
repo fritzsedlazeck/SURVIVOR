e65234f5e2764f7e8ba8bae59461bef9648abf91
fritz.sedlazeck <fritz.sedlazeck@gmail.com>
2018-02-28 09:25:04 -0600

    changed makefile

--- Debug/src/analysis_sv/subdir.mk
+++ Debug/src/analysis_sv/subdir.mk
@@ -7,6 +7,7 @@ CPP_SRCS += \
 ../src/analysis_sv/GIAB_summary.cpp \
 ../src/analysis_sv/MT_identifier.cpp \
 ../src/analysis_sv/MUMmer_overlap.cpp \
+../src/analysis_sv/Select_samples.cpp \
 ../src/analysis_sv/Simplify_SVs.cpp \
 ../src/analysis_sv/Summ_mat.cpp 
 
@@ -14,6 +15,7 @@ OBJS += \
 ./src/analysis_sv/GIAB_summary.o \
 ./src/analysis_sv/MT_identifier.o \
 ./src/analysis_sv/MUMmer_overlap.o \
+./src/analysis_sv/Select_samples.o \
 ./src/analysis_sv/Simplify_SVs.o \
 ./src/analysis_sv/Summ_mat.o 
 
@@ -21,6 +23,7 @@ CPP_DEPS += \
 ./src/analysis_sv/GIAB_summary.d \
 ./src/analysis_sv/MT_identifier.d \
 ./src/analysis_sv/MUMmer_overlap.d \
+./src/analysis_sv/Select_samples.d \
 ./src/analysis_sv/Simplify_SVs.d \
 ./src/analysis_sv/Summ_mat.d 
 
