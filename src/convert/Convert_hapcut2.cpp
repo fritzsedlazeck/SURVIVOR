0a3466c6c74add3442199d0407a4ef6683f7ea00
fritzsedlazeck <fritz.sedlazeck@gmail.com>
2019-02-26 11:30:02 -0600

    Included phase block ID in hapcut2 output

--- src/convert/Convert_hapcut2.cpp
+++ src/convert/Convert_hapcut2.cpp
@@ -7,11 +7,11 @@
 
 #include "Convert_hapcut2.h"
 
-map<std::string, bool> parse_hapcut(std::string hapcut2, std::string target_chr) {
+map<std::string, int> parse_hapcut(std::string hapcut2, std::string target_chr, int & phaseblock_id) {
 	std::string buffer;
 	std::ifstream myfile;
 
-	map<std::string, bool> hapcut;
+	map<std::string, int> hapcut;
 
 	myfile.open(hapcut2.c_str(), std::ifstream::in);
 	if (!myfile.good()) {
@@ -55,12 +55,17 @@ map<std::string, bool> parse_hapcut(std::string hapcut2, std::string target_chr)
 					ss << pos;
 
 					if (hapcut.find(ss.str()) == hapcut.end()) {
-						hapcut[ss.str()] = first_gt;
+						hapcut[ss.str()] = phaseblock_id;
+						if (!first_gt) { //negative ID for none
+							hapcut[ss.str()] = hapcut[ss.str()] * -1;
+						}
 					} else {
 						cerr << "A position was found twice: " << ss.str() << endl;
 					}
 				}
 			}
+		} else if(buffer[0] != 'B') {
+			phaseblock_id++;
 		}
 		getline(myfile, buffer);
 	}
@@ -80,12 +85,16 @@ void process_hapcut(std::string orig_snp, std::string hapcut2, std::string outpu
 		exit(0);
 	}
 	FILE * file = fopen(output.c_str(), "w");
-	map<std::string, bool> hapcut_res;
+	map<std::string, int> hapcut_res;
 	std::string old_chr;
 	getline(myfile, buffer);
+	int phaseblock = 1;
 	//int num = 0;
 	while (!myfile.eof()) {
 		if (buffer[0] == '#') {
+			if (buffer[1] == 'C') {
+				fprintf(file, "%s", "##FORMAT=<ID=PS,Number=1,Type=Integer,Description=\"Phase set identifier\">");
+			}
 			fprintf(file, "%s", buffer.c_str());
 			fprintf(file, "%c", '\n');
 		} else {
@@ -93,10 +102,10 @@ void process_hapcut(std::string orig_snp, std::string hapcut2, std::string outpu
 
 			std::size_t found = buffer.find_last_of('\t');
 			//check found + 1 if '1' && if found+3 !='0' ->
-			if ((buffer[found + 1] == '0' && buffer[found + 3] == '1') ||( buffer[found + 1] == '1' && buffer[found + 3] == '0') ) {
+			if ((buffer[found + 1] == '0' && buffer[found + 3] == '1') || (buffer[found + 1] == '1' && buffer[found + 3] == '0')) {
 				//search pos and replace 3 chars.
 				int count = 0;
-				std::string chr;
+				std::string chr = "";
 				int pos = 0;
 				for (size_t i = 0; i < buffer.size(); i++) {
 					if (count == 0 && buffer[i] != '\t') {
@@ -113,20 +122,23 @@ void process_hapcut(std::string orig_snp, std::string hapcut2, std::string outpu
 				if (strcmp(chr.c_str(), old_chr.c_str()) != 0) {
 					//load new chr set:
 					cout << "Parsing hapcut2 output for " << chr;
-					hapcut_res = parse_hapcut(hapcut2, chr);
+					hapcut_res = parse_hapcut(hapcut2, chr, phaseblock);
 					cout << " SNPs parsed " << hapcut_res.size() << endl;
 					old_chr = chr;
-
 				}
 
 				if (!chr.empty()) {
+
 					std::stringstream ss;
 					ss << chr;
 					ss << "_";
 					ss << pos;
 					if (hapcut_res.find(ss.str()) != hapcut_res.end()) {
-			//			cout << "MATCH: " << ss.str() << endl;
-						if (hapcut_res[ss.str()]) {
+
+						buffer.insert(found, ":PS");
+						found += 3;
+						//			cout << "MATCH: " << ss.str() << endl;
+						if (hapcut_res[ss.str()] > 0) {
 							buffer[found + 1] = '1';
 							buffer[found + 2] = '|';
 							buffer[found + 3] = '0';
@@ -135,6 +147,11 @@ void process_hapcut(std::string orig_snp, std::string hapcut2, std::string outpu
 							buffer[found + 2] = '|';
 							buffer[found + 3] = '1';
 						}
+						std::stringstream id;
+						id << ":";
+						id << abs(hapcut_res[ss.str()]);
+
+						buffer.append(id.str());
 
 					}
 				}
